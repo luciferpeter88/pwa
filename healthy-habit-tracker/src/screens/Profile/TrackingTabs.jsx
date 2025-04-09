9c64225e824ce3eff9d6ed7e607cfbff755b35ac
luciferpeter88 <kaszapnagyp@gmail.com>
2025-04-09 14:24:41 +0100

    .

--- healthy-habit-tracker/src/screens/Profile/TrackingTabs.jsx
+++ healthy-habit-tracker/src/screens/Profile/TrackingTabs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const TrackingTabs = () => {
   const tabs = [
@@ -16,14 +16,18 @@ const TrackingTabs = () => {
       route: "/profile/steps",
     },
   ];
-
+  const { pathname } = useLocation();
   return (
-    <nav className="flex p-1.5 mt-5 bg-[#232828] rounded-full">
+    <nav className="flex  mt-5 bg-[#232828] rounded-full">
       {tabs.map((tab) => (
         <Link to={tab.route} className="flex-1" key={tab.header}>
           <button
             key={tab.header}
-            className="flex-1 px-7 py-3 text-xs text-center text-white cursor-pointer rounded-full"
+            className={`flex-1 px-7 py-3 text-xs text-center text-white cursor-pointer rounded-full ${
+              pathname === tab.route
+                ? "bg-[#f88415] text-white"
+                : "bg-[#232828] text-[#f1f1f1]"
+            }`}
             aria-label={`Switch to ${tab} tracking`}
           >
             {tab.header}
